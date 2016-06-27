15b25334ee7d2cd80d5e832a028ec2084a042828
王梓键 <wangzj@mama.cn>
2016-06-27 08:25:19 +0800

    城市信息

--- www/common_districts.sql
+++ www/common_districts.sql
@@ -7,18 +7,6 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.24)
 # Database: py_duodian
-# Generation Time: 2016-06-26 05:37:51 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 # Dump of table task_common_district
 # ------------------------------------------------------------
